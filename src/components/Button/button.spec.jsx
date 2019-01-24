79a997bfcba5623427d3a30151137530a60a68a6
dbalas <dbalasdev@gmail.com>
2019-01-24 09:25:40 +0100

    configuring jest with enzyme

--- src/components/Button/button.spec.jsx
+++ src/components/Button/button.spec.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { HoiPoiProvider } from '../../utils/styles';
 import Button from '../Button';
 import renderer from 'react-test-renderer';
 
 describe('Button', () => {
     test('is rendered without crashes', () => {
-        renderer.create(<Button>Hello</Button>);
+        renderer.create(
+            <HoiPoiProvider>
+                <Button>Hello</Button>
+            </HoiPoiProvider>,
+        );
     });
 });
