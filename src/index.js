334cefa3d2b97dab0e2f4a4c74ccd9ca29c49820
dbalas <dbalasdev@gmail.com>
2019-04-08 12:34:50 +0200

    fixing Loader export

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ export { HoiPoiProvider, createHoiPoiTheme } from './utils/styles';
 
 // Components
 export { default as Button } from './components/Button';
+export { default as Loader } from './components/Loader';
