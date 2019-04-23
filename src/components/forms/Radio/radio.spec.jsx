58cfd47179c4b93658fa08c7d270403ee18631fb
Daniel Balastegui <dbalasdev@gmail.com>
2019-04-23 20:31:07 +0200

    Update radio.spec.jsx

--- src/components/forms/Radio/radio.spec.jsx
+++ src/components/forms/Radio/radio.spec.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import toJson from 'enzyme-to-json';
-import { HoiPoiProvider } from 'utils/styles';
-import Radio from 'components/forms/Radio';
+import { HoiPoiProvider } from '../../../utils/styles';
+import Radio from './Radio';
 
 describe('Radio', () => {
     test('unchecked match snapshot', () => {
