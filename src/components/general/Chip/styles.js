4b91d20360d5b056ae4d1f4f8b910cba6ddeb0cb
jordi.gandia <jordi.gandia@forcemanager.net>
2019-05-16 14:55:15 +0200

    updating test

--- src/components/general/Chip/styles.js
+++ src/components/general/Chip/styles.js
@@ -1,15 +1,19 @@
 export default (theme) => ({
     root: {
         ...theme.typography.smallText,
-        lineHeight: '20px',
-        borderRadius: '3px',
-        display: 'inline-block',
-        verticalAlign: 'middle',
-        padding: '0',
-        height: '20px',
-        marginRight: '3px',
         backgroundColor: theme.colors.secondaryGrey,
         color: theme.colors.text.greySoft,
+
+        borderRadius: '3px',
+
+        lineHeight: '20px',
+
+        display: 'inline-block',
+        verticalAlign: 'middle',
+
+        padding: '0',
+        marginRight: '3px',
+        height: '20px',
     },
 
     wrapper: {
@@ -18,22 +22,26 @@ export default (theme) => ({
     },
 
     text: {
-        display: 'flex',
+        color: theme.colors.text.greySoft,
+
         fontSize: '13px',
         lineHeight: '20px',
         letterSpacing: '0.5px',
+
+        display: 'flex',
         padding: '0 5px',
-        color: theme.colors.text.greySoft,
     },
 
     shrinked: {
+        color: theme.colors.text.greySoft,
+
         fontSize: '11px',
-        height: '20px',
-        width: '20px',
         lineHeight: '20px',
         letterSpacing: '0.5px',
         textAlign: 'center',
-        color: theme.colors.text.greySoft,
+
+        height: '20px',
+        width: '20px',
     },
 
     close: {
@@ -44,13 +52,15 @@ export default (theme) => ({
     picture: {
         height: '20px',
         width: '20px',
+
         borderTopLeftRadius: '3px',
         borderBottomLeftRadius: '3px',
     },
 
     shrinkedPicture: {
+        borderRadius: '3px',
+
         height: '20px',
         width: '20px',
-        borderRadius: '3px',
     },
 });
