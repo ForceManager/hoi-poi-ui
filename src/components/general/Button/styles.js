2bd7c03b1d70f366a9ac717096e665057663fa5c
dbalas <dbalasdev@gmail.com>
2019-09-06 11:51:31 +0200

    Fixing button isLoading with hover

--- src/components/general/Button/styles.js
+++ src/components/general/Button/styles.js
@@ -20,9 +20,6 @@ export default (theme) => ({
         '&$disabled': {
             opacity: 0.5,
             cursor: 'not-allowed !important',
-            '&:hover': {
-                backgroundColor: theme.colors.primary,
-            },
             '&:active': {
                 boxShadow: 'none',
             },
@@ -120,7 +117,7 @@ export default (theme) => ({
             transition: `box-shadow .2s ease-in, background-color .1s ease-in-out`,
             '&$disabled': {
                 '&:hover': {
-                    backgroundColor: 'transparent',
+                    backgroundColor: 'transparent !important',
                 },
             },
             '& $Text': {
@@ -134,7 +131,7 @@ export default (theme) => ({
             backgroundColor: theme.colors.primary,
             '&$disabled': {
                 '&:hover': {
-                    backgroundColor: theme.colors.primary,
+                    backgroundColor: `${theme.colors.primary} !important`,
                 },
             },
             '&:hover': {
@@ -228,6 +225,11 @@ export default (theme) => ({
         '& $Text': {
             color: theme.colors.text.default,
         },
+        '&$disabled': {
+            '&:hover': {
+                backgroundColor: 'transparent',
+            },
+        },
         '&:hover': {
             backgroundColor: theme.colors.greyLight,
         },
@@ -238,6 +240,11 @@ export default (theme) => ({
         '& $Text': {
             color: theme.colors.text.primary,
         },
+        '&$disabled': {
+            '&:hover': {
+                backgroundColor: theme.colors.primary,
+            },
+        },
         '&:hover': {
             backgroundColor: theme.colors.primarySoft,
         },
@@ -247,6 +254,11 @@ export default (theme) => ({
         transition: `box-shadow .2s ease-in, background-color .1s ease-in-out`,
         '& $Text': {
             color: theme.colors.text.white,
+        },
+        '&$disabled': {
+            '&:hover': {
+                backgroundColor: theme.colors.red,
+            },
         },
         '&:hover': {
             backgroundColor: theme.colors.redSoft,
