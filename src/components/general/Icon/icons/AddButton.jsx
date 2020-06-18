65abc0e92d1694c681c73fed111506f1a68d2798
Sergi Palau <sergi.palau@forcemanager.net>
2020-06-18 15:22:10 +0200

    add & less buttons design update

--- src/components/general/Icon/icons/AddButton.jsx
+++ src/components/general/Icon/icons/AddButton.jsx
@@ -2,11 +2,10 @@ import React from 'react';
 
 function AddButton({ color = '#788590', ...props }) {
     return (
-        <svg viewBox="0 0 18 18" {...props}>
+        <svg viewBox="0 0 24 24" {...props}>
             <g fill="none" fillRule="evenodd">
-                <rect stroke="#CED4D9" x={0.5} y={0.5} width={17} height={17} rx={2} />
                 <path
-                    d="M9 4a1 1 0 011 1v3h3a1 1 0 01.993.883L14 9a1 1 0 01-1 1h-3v3a1 1 0 01-.883.993L9 14a1 1 0 01-1-1v-3H5a1 1 0 01-.993-.883L4 9a1 1 0 011-1h3V5a1 1 0 01.883-.993z"
+                    d="M13 7h-2v4H7v2h4v4h2v-4h4v-2h-4V7zm-1-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"
                     fill={color}
                 />
             </g>
