f2521c72c9f0acdfd94e382da1323d3848a46b89
Sergi Palau <sergi.palau@forcemanager.net>
2020-06-25 12:00:01 +0200

    Users icons fixes

--- src/components/general/Icon/icons/AddUser.jsx
+++ src/components/general/Icon/icons/AddUser.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 function AddUser({ color = '#788590', ...props }) {
     return (
-        <svg viewBox="0 0 22 16" fill="none" {...props}>
+        <svg viewBox="0 0 24 24" fill="none" id="Icons/Social/ic_person_add" {...props}>
             <path
                 fillRule="evenodd"
                 clipRule="evenodd"
-                d="M14 8c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zM5 6V3H3v3H0v2h3v3h2V8h3V6H5zm9 4c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"
+                d="M15 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm-9-2V7H4v3H1v2h3v3h2v-3h3v-2H6zm9 4c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"
                 fill={color}
             />
         </svg>
--- src/components/general/Icon/icons/SingleUser.jsx
+++ src/components/general/Icon/icons/SingleUser.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 function SingleUser({ color = '#788590', ...props }) {
     return (
-        <svg viewBox="0 0 16 16" fill="none" {...props}>
+        <svg viewBox="0 0 24 24" fill="none" id="AndWeb/Social/ic_person" {...props}>
             <path
                 fillRule="evenodd"
                 clipRule="evenodd"
-                d="M8 8c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"
+                d="M12 12.0001C14.21 12.0001 16 10.2101 16 8.00009C16 5.79009 14.21 4.00009 12 4.00009C9.79 4.00009 8 5.79009 8 8.00009C8 10.2101 9.79 12.0001 12 12.0001ZM12 14.0001C9.33 14.0001 4 15.3401 4 18.0001V20.0001H20V18.0001C20 15.3401 14.67 14.0001 12 14.0001Z"
                 fill={color}
             />
         </svg>
