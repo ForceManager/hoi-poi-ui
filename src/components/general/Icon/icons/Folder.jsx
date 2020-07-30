3f0fd1e050ff4d8aba1cebad3c4cea5083665ed3
Eduard Trias <eduard.trias@forcemanager.net>
2020-07-30 19:07:43 +0200

    change folder icon and add build:watch command

--- src/components/general/Icon/icons/Folder.jsx
+++ src/components/general/Icon/icons/Folder.jsx
@@ -1,20 +1,12 @@
 import React from 'react';
 
-const Folder = () => (
-    <svg viewBox="0 0 18 18" version="1.1" xmlns="http://www.w3.org/2000/svg">
-        <g
-            id="Svg-Icons/18px-(All-icons)/folder_small"
-            stroke="none"
-            strokeWidth="1"
-            fill="none"
+const Folder = ({ color, ...props }) => (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+        <path
+            d="M10 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2h-8l-2-2z"
+            fill={color}
             fillRule="evenodd"
-        >
-            <path
-                d="M9.06523157,4.10131836 L8.15564253,2 L2.66262231,2 C1.74572733,2 1,2.71706093 1,3.59870968 L1,14.4193548 C1,15.2909677 1.73737769,16 2.64435747,16 L15.3305936,16 C16.2511416,16 17,15.2799283 17,14.394767 L17,5.70604954 C17,4.82139004 16.2511416,4.10131836 15.3311155,4.10131836"
-                id="{Icon}"
-                fill="#788590"
-            />
-        </g>
+        />
     </svg>
 );
 
