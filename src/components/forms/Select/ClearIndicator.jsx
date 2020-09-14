9882e7d638874074cd2fa6ebdd0bd18a65381a70
Sergi Palau <sergi.palau@forcemanager.net>
2020-09-14 10:46:29 +0200

    console.log remove

--- src/components/forms/Select/ClearIndicator.jsx
+++ src/components/forms/Select/ClearIndicator.jsx
@@ -5,7 +5,6 @@ import Icon from '../../general/Icon';
 export default (hide, className) => {
     if (hide) return null;
     return React.memo((props) => {
-        console.log('ClearIndicator', hide, className, props);
         return (
             components.ClearIndicator && (
                 <components.ClearIndicator {...props} className={className}>
