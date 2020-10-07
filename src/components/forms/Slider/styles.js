4110883d04399a93d4740540f635b432984097a1
Jordi Gandia <jordi.gandia@forcemanager.net>
2020-10-07 08:16:03 +0200

    Files added

---
+++ src/components/forms/Slider/styles.js
@@ -0,0 +1,192 @@
+export default (theme) => ({
+    root: {},
+    Label: {
+        width: 153,
+        marginRight: 17,
+        padding: '6px 0',
+    },
+    error: {
+        ...theme.typography.defaultText,
+        position: 'absolute',
+        bottom: -15,
+        color: theme.colors.red,
+    },
+    formControl: {
+        position: 'relative',
+        display: 'flex',
+        alignItems: 'center',
+        width: 290,
+        lineHeight: 0,
+    },
+    errored: {
+        paddingBottom: '20px !important',
+    },
+    vertical: {
+        display: 'block',
+        padding: '12px 0 11px 0',
+        marginLeft: '0 !important',
+        '& $formControl': {
+            marginTop: 10,
+        },
+    },
+    horizontal: {
+        display: 'flex',
+        alignItems: 'center',
+        padding: '12px 0 11px 0',
+        '& $error': {},
+    },
+    isFullWidth: {
+        '& $formControl': {
+            width: '100%',
+            flex: 1,
+        },
+    },
+    // Slider custom styles
+    slider: {
+        width: 'calc(100% - 15px)',
+        minHeight: 50,
+        margin: 'auto',
+        position: 'relative',
+        padding: '25px 0',
+        borderRadius: 6,
+        touchAction: 'none',
+        boxSizing: 'border-box',
+        '& .rc-slider-rail': {
+            boxSizing: 'border-box',
+            position: 'absolute',
+            width: '100%',
+            backgroundColor: theme.colors.neutral400,
+            height: 4,
+            borderRadius: 6,
+        },
+        '& .rc-slider-track': {
+            boxSizing: 'border-box',
+            position: 'absolute',
+            left: '0',
+            height: 4,
+            borderRadius: 6,
+            backgroundColor: theme.colors.orange500,
+        },
+        '& .rc-slider-step': {
+            boxSizing: 'border-box',
+            position: 'absolute',
+            width: '100%',
+            height: 16,
+            background: 'transparent',
+        },
+    },
+
+    smallSlider: {
+        width: 'calc(100% - 15px)',
+        minHeight: 50,
+        margin: 'auto',
+        position: 'relative',
+        padding: '25px 0',
+        borderRadius: 6,
+        touchAction: 'none',
+        boxSizing: 'border-box',
+        '& .rc-slider-rail': {
+            boxSizing: 'border-box',
+            position: 'absolute',
+            width: '100%',
+            backgroundColor: theme.colors.greyLight,
+            height: 2,
+            borderRadius: 6,
+        },
+        '& .rc-slider-track': {
+            boxSizing: 'border-box',
+            position: 'absolute',
+            left: '0',
+            height: 2,
+            borderRadius: 6,
+            backgroundColor: theme.colors.orange500,
+        },
+        '& .rc-slider-step': {
+            boxSizing: 'border-box',
+            position: 'absolute',
+            width: '100%',
+            height: 4,
+            background: 'transparent',
+        },
+    },
+    overlay: {},
+    overlayHandler: {
+        position: 'absolute',
+        marginLeft: -6,
+        marginTop: -6,
+        width: 16,
+        height: 16,
+        cursor: 'grab',
+        borderRadius: '50%',
+        border: '0',
+        backgroundColor: theme.colors.orange500,
+        MsTouchAction: 'pan-x',
+        touchAction: 'pan-x',
+        '&:active': {
+            cursor: 'grabbing',
+            border: `solid 4px ${theme.colors.orange100}`,
+            marginTop: -10,
+        },
+        '&:focus': {
+            outline: 'none',
+        },
+        '&:hover': {
+            border: `solid 4px ${theme.colors.orange100}`,
+            marginTop: -10,
+        },
+    },
+
+    smallOverlayHandler: {
+        position: 'absolute',
+        marginLeft: -5,
+        marginTop: -5,
+        width: 12,
+        height: 12,
+        cursor: 'grab',
+        borderRadius: '50%',
+        border: '0',
+        backgroundColor: theme.colors.orange500,
+        MsTouchAction: 'pan-x',
+        touchAction: 'pan-x',
+        '&:active': {
+            cursor: 'grabbing',
+            border: `solid 3px ${theme.colors.orange100}`,
+            marginTop: -8,
+        },
+        '&:focus': {
+            outline: 'none',
+        },
+        '&:hover': {
+            border: `solid 3px ${theme.colors.orange100}`,
+            marginTop: -8,
+        },
+    },
+    overlayLabel: {
+        position: 'absolute',
+        minWidth: 15,
+        top: 4,
+        marginLeft: -15,
+        textAlign: 'center',
+        color: theme.colors.greySoft,
+        ...theme.typography.smallText,
+    },
+    isReadOnly: {
+        '& $overlayHandler': {
+            backgroundColor: theme.colors.neutral500,
+            cursor: 'default',
+            '&:hover': {
+                border: 0,
+            },
+        },
+        '& $smallOverlayHandler': {
+            backgroundColor: theme.colors.neutral500,
+            cursor: 'default',
+            '&:hover': {
+                border: 0,
+            },
+        },
+        '& .rc-slider-track': {
+            backgroundColor: theme.colors.neutral500,
+        },
+    },
+});
