9a6e4f5bf782f9aecd788eac522dd69a0dda746e
Eduard Trias <eduard.trias@forcemanager.net>
2020-10-07 12:35:23 +0200

    add InsertDriveFile icon

---
+++ src/components/general/Icon/icons/editor/InsertDriveFile.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function InsertDriveFile({ color = '#788590', ...props }) {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <path
+                d="M6 2c-1.1 0-1.99.9-1.99 2L4 20c0 1.1.89 2 1.99 2H18c1.1 0 2-.9 2-2V8l-6-6H6zm7 7V3.5L18.5 9H13z"
+                fill={color}
+                fillRule="evenodd"
+            />
+        </svg>
+    );
+}
+
+export default InsertDriveFile;
