aa87605124239e9c6c3dde055b75a4b12042c2c3
Sergi Palau <sergi.palau@forcemanager.net>
2020-12-10 11:29:13 +0100

    Add next icon-drag

--- src/components/general/Icon/index.jsx
+++ src/components/general/Icon/index.jsx
@@ -46,6 +46,7 @@ import VideoCamera from './icons/VideoCamera';
 import Chat from './icons/Chat';
 import ChatFilled from './icons/ChatFilled';
 import Send from './icons/Send';
+import DraggableSort from './icons/DraggableSort';
 
 //Actions
 import AccountCheckin from './icons/actions/AccountCheckin';
@@ -231,6 +232,7 @@ const ICONS = {
     videoCamera: VideoCamera,
     chat: Chat,
     chatFilled: ChatFilled,
+    draggableSort: DraggableSort,
     //Actions
     accountCheckin: AccountCheckin,
     accountDetails: AccountDetails,
