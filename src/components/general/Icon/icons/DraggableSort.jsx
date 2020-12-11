6d1e70b47606b0bef2d7e9023fee45e99683a3d8
Sergi Palau <sergi.palau@forcemanager.net>
2020-12-11 12:23:15 +0100

    fix svg DraggableSort

--- src/components/general/Icon/icons/DraggableSort.jsx
+++ src/components/general/Icon/icons/DraggableSort.jsx
@@ -4,8 +4,8 @@ function DraggableSort({ color = '#788590', ...props }) {
     return (
         <svg viewBox="0 0 24 24" {...props}>
             <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
+                fillRule="evenodd"
+                clipRule="evenodd"
                 d="M12 2l3 3H9l3-3zM2 15v2h20v-2H2zm0-4v2h20v-2H2zm0-4v2h20V7H2zm7 12l3 3 3-3H9z"
                 fill={color}
             />
