1c0bf16727895db95578a075d19253316e1f1064
Xavier Bonell <mail@xbonell.com>
2021-02-23 11:53:26 +0100

    Added show '%n Selected' option to multiselect

---
+++ src/components/forms/Select/components/ValueContainer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { components } from 'react-select';
+
+export default React.memo(({ children, selectProps, ...props }) => {
+    const { numSelectedLiteral } = selectProps;
+    let [values, input] = children;
+
+    if (Array.isArray(values)) {
+        values = numSelectedLiteral.replace('%@', values.length);
+    }
+
+    return (
+        <components.ValueContainer {...props}>
+            {values}
+            {input}
+        </components.ValueContainer>
+    );
+});
