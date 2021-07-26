5f2c21a3f6e6b8c205fd599a8da134496c8d6c8c
Eduard Trias <eduard.trias@forcemanager.net>
2021-07-26 17:00:26 +0200

    add agenda icons

---
+++ src/components/general/Icon/icons/actions/CalendarViewWorkDay.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function CalendarViewWorkDay({ color = '#788590', ...props }) {
+    return (
+        <svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}>
+            <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M18 4h1c1.1 0 2 .9 2 2v14c0 1.1-.9 2-2 2H5c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2h1V2h2v2h8V2h2v2zM5 20h14v-9H5v9zM5 9h14V6H5v3zm1 3h12v4H6v-4zm1 1h10v2H7v-2z"
+                fill={color}
+            />
+        </svg>
+    );
+}
+
+export default CalendarViewWorkDay;
