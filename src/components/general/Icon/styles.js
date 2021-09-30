9a38dbfb289a1b72b62ac55a746703da7cf625bd
daniel.balastegui <daniel.balastegui@forcemanager.net>
2021-09-30 08:13:16 +0200

    Fixing icon size for safari

--- src/components/general/Icon/styles.js
+++ src/components/general/Icon/styles.js
@@ -7,22 +7,42 @@ export default (theme) => ({
     small: {
         width: 10,
         height: 10,
+        '& svg': {
+            width: 10,
+            height: 10,
+        },
     },
     medium: {
         width: 18,
         height: 18,
+        '& svg': {
+            width: 18,
+            height: 18,
+        },
     },
     large: {
         width: 24,
         height: 24,
+        '& svg': {
+            width: 24,
+            height: 24,
+        },
     },
     big: {
         width: 32,
         height: 32,
+        '& svg': {
+            width: 32,
+            height: 32,
+        },
     },
     huge: {
         width: 64,
         height: 64,
+        '& svg': {
+            width: 64,
+            height: 64,
+        },
     },
     raw: {
         width: 'inherit',
