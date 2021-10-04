45cc0820feef9602383a3df2a22360f3ed44c28d
daniel.balastegui <daniel.balastegui@forcemanager.net>
2021-10-04 16:13:31 +0200

    updating and husky config

--- jest/fileTransform.js
+++ jest/fileTransform.js
@@ -2,4 +2,4 @@ const config = {
     babelrc: false,
     presets: ['@babel/preset-env', '@babel/preset-react'],
 };
-module.exports = require('babel-jest').createTransformer(config);
+module.exports = require('babel-jest').default.createTransformer(config);
