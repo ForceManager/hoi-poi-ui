5a09e41ba50e49dfc7bee2ea0a0c935f7f76429a
Xavier Bonell <xavier.bonell@forcemanager.net>
2022-06-07 12:17:39 +0200

    Allow to pass secondary color as a prop

--- src/components/general/Icon/icons/communication/EmailSend.jsx
+++ src/components/general/Icon/icons/communication/EmailSend.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-function EmailSend({ color = '#788590', ...props }) {
+function EmailSend({ color = '#788590', color2 = '#FF8C00', ...props }) {
     return (
         <svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}>
             <path
                 d="M20 4H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h7.35a5.8 5.8 0 0 1-.35-2 6 6 0 0 1 11-3.31V6a2 2 0 0 0-2-2Zm0 4-8 5-8-5V6l8 5 8-5v2Z"
                 fill={color}
             />
-            <path d="M13 17h4v-3l5 4.5-5 4.5v-3h-4v-3Z" fill="#00A8BE" />
+            <path d="M13 17h4v-3l5 4.5-5 4.5v-3h-4v-3Z" fill={color2} />
         </svg>
     );
 }
