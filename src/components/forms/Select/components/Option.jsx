c08f6204365a2aa0c1ae542a6cbe2707126e71fa
Xavier Bonell <xavier.bonell@forcemanager.net>
2022-08-01 11:28:36 +0200

    Added select with dividers and divider component

--- src/components/forms/Select/components/Option.jsx
+++ src/components/forms/Select/components/Option.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { components } from 'react-select';
+import Divider from '../../../general/Divider';
 
 export default ({ className, override }) => {
     return React.memo(({ children, ...props }) => {
-        return (
+        return !props?.data?.divider ? (
             components.Option && (
                 <components.Option {...props} className={className} {...override.option}>
                     {children}
                 </components.Option>
             )
+        ) : (
+            <Divider />
         );
     });
 };
