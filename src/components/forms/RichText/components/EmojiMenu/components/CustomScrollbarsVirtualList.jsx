2bb8abaf2277d3ae7c0a4866a5b4e88216bdab4c
Xavier Bonell <xavier.bonell@forcemanager.net>
2022-12-24 20:00:51 +0100

    RichText emoji menu (WIP)

---
+++ src/components/forms/RichText/components/EmojiMenu/components/CustomScrollbarsVirtualList.jsx
@@ -0,0 +1,27 @@
+import React, { useCallback, forwardRef, memo } from 'react';
+import ScrollBar from '../../../../../utils/ScrollBar';
+
+const CustomScrollbars = ({ onScroll, forwardedRef, style, children }) => {
+    const refSetter = useCallback(
+        (scrollbarsRef) => {
+            if (scrollbarsRef) {
+                forwardedRef(scrollbarsRef.view);
+            } else {
+                forwardedRef(null);
+            }
+        },
+        [forwardedRef],
+    );
+
+    return (
+        <ScrollBar ref={refSetter} style={{ ...style, overflow: 'hidden' }} onScroll={onScroll}>
+            {children}
+        </ScrollBar>
+    );
+};
+
+const CustomScrollbarsVirtualList = forwardRef((props, ref) => (
+    <CustomScrollbars {...props} forwardedRef={ref} />
+));
+
+export default memo(CustomScrollbarsVirtualList);
