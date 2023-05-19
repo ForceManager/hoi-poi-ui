cb8e9bcf8a873cf1716a0b8735e06b5fbb025bd0
Xavier Bonell <xbonell@gmail.com>
2023-05-19 10:15:30 +0200

    Enhanced link component for Markdown

--- src/components/general/Markdown/components/List.jsx
+++ src/components/general/Markdown/components/List.jsx
@@ -5,7 +5,7 @@ const List = ({ items, ...otherProps }) => {
     const listProps = useMemo(() => {
         const finalItems = items
             .filter((item) => item.type === 'li')
-            .map((item) => item?.props?.children?.[0] || null);
+            .map((item) => item?.props?.children || null);
         return { items: finalItems, ...otherProps };
     }, [items, otherProps]);
 
