c4c2664ce2b60987a4f7b393348da1f56dbe402b
Jordi Gandia <forcemanager@outlook.com>
2023-11-13 16:29:02 +0100

    Adding three new icons

---
+++ src/components/general/Icon/icons/Logout.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const Logout = ({ color = '#7D8A96' }) => (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none">
+        <path
+            fill={color}
+            d="m17 7-1.41 1.41L18.17 11H8v2h10.17l-2.58 2.58L17 17l5-5-5-5ZM4 5h8V3H4c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h8v-2H4V5Z"
+        />
+    </svg>
+);
+export default Logout;
