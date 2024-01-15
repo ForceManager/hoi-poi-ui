1f3d1e08a6ba9ef2903a552b0dc55cf5a5f580e7
daniel.balastegui <daniel.balastegui@forcemanager.net>
2024-01-15 10:23:09 +0100

    fixing carousel tests

--- src/components/general/Carousel/carousel.spec.jsx
+++ src/components/general/Carousel/carousel.spec.jsx
@@ -8,7 +8,27 @@ describe('Carousel', () => {
     test('default match', () => {
         const wrapper = mount(
             <HoiPoiProvider>
-                <Carousel />
+                <Carousel
+                    placeholder="https://static.vecteezy.com/system/resources/previews/004/141/669/non_2x/no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-coming-soon-sign-simple-nature-silhouette-in-frame-isolated-illustration-vector.jpg"
+                    images={[
+                        {
+                            original: 'https://picsum.photos/1200/900',
+                            thumbnail: 'https://picsum.photos/1200/900',
+                        },
+                        {
+                            original: 'https://picsum.photos/1000/900',
+                            thumbnail: 'https://picsum.photos/1200/900',
+                        },
+                        {
+                            original: 'https://picsum.photos/400/400',
+                            thumbnail: 'https://picsum.photos/1200/900',
+                        },
+                        {
+                            original: 'https://picsum.photos/200/600',
+                            thumbnail: 'https://picsum.photos/1200/900',
+                        },
+                    ]}
+                />
             </HoiPoiProvider>,
         );
         expect(toJson(wrapper)).toMatchSnapshot();
