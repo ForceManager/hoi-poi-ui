9251958eba58e797750d6c7552c3b4c714c98f1d
daniel.balastegui <daniel.balastegui@forcemanager.net>
2024-01-22 17:06:33 +0100

    spacer component with some snaps

---
+++ src/components/utils/Spacer/Readme.md
@@ -0,0 +1,17 @@
+Horizontal
+
+```jsx
+<Spacer x={1} />
+```
+
+Vertical
+
+```jsx
+<Spacer y={1} />
+```
+
+### Component tree
+
+---
+
+-   root
---
+++ src/components/utils/Spacer/index.jsx
@@ -0,0 +1,32 @@
+import React, { memo } from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import { getOverrides } from '../../../utils/overrides';
+
+const Spacer = ({ x, y, overrides: overridesProp, className: classNameProp }) => {
+    // Overrides
+    const override = getOverrides(overridesProp, Spacer.overrides);
+
+    // Classes
+    const rootClassName = classnames(classNameProp);
+
+    const style = {
+        margin: `${y * 4}px ${x * 4}px`,
+    };
+
+    return <div className={rootClassName} style={style} {...override.root} />;
+};
+
+Spacer.overrides = ['root'];
+
+Spacer.defaultProps = {
+    x: 0,
+    y: 0,
+};
+
+Spacer.propTypes = {
+    x: PropTypes.number,
+    y: PropTypes.number,
+};
+
+export default memo(Spacer);
