b66f63d6c85d5ee8df095b3c4899dd53480de718
daniel.balastegui <daniel.balastegui@forcemanager.net>
2024-04-02 11:55:38 +0200

    Adding Variables icon

--- src/components/general/Icon/icons/actions/Variables.jsx
+++ src/components/general/Icon/icons/actions/Variables.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function EmailStar({ color = '#788590', ...props }) {
+function Variables({ color = '#788590', ...props }) {
     return (
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}>
             <path
@@ -11,4 +11,4 @@ function EmailStar({ color = '#788590', ...props }) {
     );
 }
 
-export default EmailStar;
+export default Variables;
