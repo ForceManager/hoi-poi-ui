d5b327fe796e2920817d8b7e451b302ca97b2977
Xavier Bonell <xbonell@gmail.com>
2024-12-10 13:10:08 +0100

    Refactored checkbox component to match colors to the new palette

--- src/components/general/Checkbox/index.jsx
+++ src/components/general/Checkbox/index.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useMemo } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import { getOverrides, useClasses } from '../../../utils/overrides';
@@ -9,15 +9,13 @@ import styles from './styles';
 const useStyles = createUseStyles(styles, 'Checkbox');
 
 const defaultColors = [
-    'neutral',
-    'red',
-    'orange',
-    'green',
-    'blue',
+    'grey',
+    'semanticNegative',
+    'actionMajor',
+    'semanticPositive',
+    'semanticInfo',
+    'semanticFocus',
     'purple',
-    'aqua',
-    'yellow',
-    'turquoise',
 ];
 
 function Checkbox({
@@ -27,7 +25,7 @@ function Checkbox({
     checked,
     indeterminate,
     isDisabled,
-    color: colorProp = 'neutral',
+    color: colorProp = 'grey',
     isMonotone,
     onChange = () => {},
     size = 'medium',
@@ -46,26 +44,89 @@ function Checkbox({
         classNameProp,
     );
 
+    const { colorEnabled, colorDisabled } = useMemo(() => {
+        let disabled = theme.colors.grey[200];
+        let enabled = theme.colors.textLight.secondary;
+
+        switch (colorProp) {
+            case 'grey':
+                disabled = theme.colors.grey[200];
+                enabled = theme.colors.grey[500];
+                break;
+            case 'actionMajor':
+                disabled = theme.colors.actionMajor[150];
+                enabled = theme.colors.actionMajor[500];
+                break;
+            case 'semanticPositive':
+                disabled = theme.colors.semantic.positiveCustom200;
+                enabled = theme.colors.semantic.positive500;
+                break;
+            case 'semanticNegative':
+                disabled = theme.colors.semantic.negativeCustom200;
+                enabled = theme.colors.semantic.negative500;
+                break;
+            case 'semanticInfo':
+                disabled = theme.colors.semantic.infoCustom100;
+                enabled = theme.colors.semantic.info500;
+                break;
+            case 'semanticFocus':
+                disabled = theme.colors.semantic.focusCustom200;
+                enabled = theme.colors.semantic.focus500;
+                break;
+            case 'purple':
+                disabled = theme.colors.temp.purple200;
+                enabled = theme.colors.temp.purple500;
+                break;
+            default:
+                break;
+        }
+
+        return { colorEnabled: enabled, colorDisabled: disabled };
+    }, [
+        colorProp,
+        theme.colors.actionMajor,
+        theme.colors.grey,
+        theme.colors.semantic.focus500,
+        theme.colors.semantic.focusCustom200,
+        theme.colors.semantic.info500,
+        theme.colors.semantic.infoCustom100,
+        theme.colors.semantic.negative500,
+        theme.colors.semantic.negativeCustom200,
+        theme.colors.semantic.positive500,
+        theme.colors.semantic.positiveCustom200,
+        theme.colors.temp.purple200,
+        theme.colors.temp.purple500,
+        theme.colors.textLight.secondary,
+    ]);
+
     const getColor = useCallback(
         (checkState) => {
             if (isMonotone) {
-                if (colorProp === 'neutral') {
+                if (colorProp === 'grey') {
                     return isDisabled ? theme.colors.grey[200] : theme.colors.textLight.secondary;
                 }
                 return isDisabled
-                    ? theme.colors[`${colorProp}200`] || theme.colors.grey[200]
-                    : theme.colors[`${colorProp}500`] || theme.colors.textLight.secondary;
+                    ? colorDisabled || theme.colors.grey[200]
+                    : colorEnabled || theme.colors.textLight.secondary;
             } else {
                 if (checkState === 'unchecked') {
                     return isDisabled ? theme.colors.grey[200] : theme.colors.textLight.secondary;
                 } else {
                     return isDisabled
-                        ? theme.colors[`${colorProp}200`] || theme.colors.grey[200]
-                        : theme.colors[`${colorProp}500`] || theme.colors.textLight.secondary;
+                        ? colorDisabled || theme.colors.grey[200]
+                        : colorEnabled || theme.colors.textLight.secondary;
                 }
             }
         },
-        [colorProp, isDisabled, theme.colors, isMonotone],
+        [
+            isMonotone,
+            colorProp,
+            isDisabled,
+            colorDisabled,
+            theme.colors.grey,
+            theme.colors.textLight.secondary,
+            colorEnabled,
+        ],
     );
 
     const rootProps = {
